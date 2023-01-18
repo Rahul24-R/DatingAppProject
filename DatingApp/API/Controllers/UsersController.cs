910597a409b3d53c0ff8f02c861cb635870ab245
Rahul24-R <beingunique2000@gmail.com>
2023-01-18 21:41:13 +0530

    API

---
+++ DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]                 //indicate that this is used for HTTP api response
+    [Route("api/[controller]")]                 // localhost/api/user - where [controller] is a place holer and will be like usercontroller
+    public class UsersController:ControllerBase //when an api is called this class object is created
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context) //datacontext creates a session with the DB as a service as mentioned in the program.cs
+        {
+           this._context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers(){ //action result is used to handle API responses like 404 not found etc..
+
+            var user = _context.Users.ToList();
+            if(user==null){
+                return NotFound();
+            }
+            return user;
+        }
+        [HttpGet("{id}")]       //specific user
+        public ActionResult<AppUser> GetUser(int id){
+            return _context.Users.Find(id);
+        }
+    }
+}
