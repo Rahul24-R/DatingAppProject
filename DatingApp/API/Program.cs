41b7a8b18d1d71ea532b9347a314291de02ebd65
Rahul24-R <beingunique2000@gmail.com>
2023-01-24 20:35:25 +0530

    cors support and displaying the api data

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -9,11 +9,11 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {                                 //adding DataContxt as a service
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));  //reading the connection string from config file
 });
-
-
+builder.Services.AddCors();  
+   //adding cors service
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));      //allowing alltype of header and methods
 app.MapControllers();
 
 app.Run();
