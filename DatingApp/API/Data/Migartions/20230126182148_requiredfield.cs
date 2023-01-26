b705c846affb0776be01d4bc693eec88320e3d9e
Rahul24-R <beingunique2000@gmail.com>
2023-01-26 23:53:03 +0530

    validation

---
+++ DatingApp/API/Data/Migartions/20230126182148_requiredfield.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migartions
+{
+    /// <inheritdoc />
+    public partial class requiredfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
