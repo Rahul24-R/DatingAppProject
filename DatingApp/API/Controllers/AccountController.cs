cc9a9444d9e8c2b2bded64f96174a152fe46979a
Rahul24-R <beingunique2000@gmail.com>
2023-01-26 23:41:59 +0530

    registerDto

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -1,8 +1,11 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTO;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Controllers
 {
     public class AccountController:BaseApiController
@@ -14,14 +17,33 @@ namespace API.Controllers
             this.context = context;
             
         }
+        // [HttpPost("register")]      //api/account/register?username=abcd&password=pwd
+        // public async Task<ActionResult<AppUser>> Register(string username,string password){
+
+        //     using var hmac = new HMACSHA512();      //hasing algo - randomly generated key
+        //                                             //using is used to dispose memory of the class after it is used by garbage collector
+        //     var user = new AppUser() {
+        //         UserName=username,
+        //         PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+        //         PasswordSalt=hmac.Key
+        //     };
+
+        //     context.Users.Add(user);
+        //     await context.SaveChangesAsync();
+
+        //     return user;
+        // }
         [HttpPost("register")]      //api/account/register?username=abcd&password=pwd
-        public async Task<ActionResult<AppUser>> Register(string username,string password){
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerdto){     //using objects -DTO (Data transfer Object)
+            if(await userExist(registerdto.Username)){
+                return BadRequest("User Exists");
+            }
 
             using var hmac = new HMACSHA512();      //hasing algo - randomly generated key
                                                     //using is used to dispose memory of the class after it is used by garbage collector
             var user = new AppUser() {
-                UserName=username,
-                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName=registerdto.Username.ToLower(),
+                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerdto.Password)),
                 PasswordSalt=hmac.Key
             };
 
@@ -29,6 +51,10 @@ namespace API.Controllers
             await context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> userExist(string username){
+            return await context.Users.AnyAsync(x=>x.UserName==username.ToLower());
         }
     }
 }
