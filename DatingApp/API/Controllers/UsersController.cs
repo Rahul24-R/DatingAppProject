ac57f596d5a8df320f61ed01e114783760948cfd
Rahul24-R <beingunique2000@gmail.com>
2023-01-28 16:34:12 +0530

    middle ware jwt endpoint authentication

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -1,11 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]     //requires an token authentication to access the endpoints of this controller
     public class UsersController:BaseApiController //when an api is called this class object is created
     {
         private readonly DataContext _context;
@@ -16,6 +18,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]        //allowa accesswithout token autherization
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){ //action result is used to handle API responses like 404 not found etc..
                 //async to mke the code async and task<> where the task has a return value, if it doesnt it  async task getusers()
             var user =await _context.Users.ToListAsync();   //await to complete the task , then to proceed with next task in the method
--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +16,22 @@ builder.Services.AddDbContext<DataContext>(opt => {                             
 });
 builder.Services.AddCors();  
 builder.Services.AddScoped<ITokenService,TokenService>();
+//install package microsoft.aspnetcore.authenticatio.jwtbearer
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters=new TokenValidationParameters{ //parameters for token validation
+        ValidateIssuerSigningKey=true,          //the jwt token requires a signed issuer
+        IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),   //getting the signature key
+        ValidateIssuer=false,       // no need to validate the issues
+        ValidateAudience=false
+    };
+});
    //adding cors service
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));      //allowing alltype of header and methods
+app.UseAuthentication(); //do u have  valid token -middleware
+app.UseAuthorization(); //what u are allowed to do-middleware
 app.MapControllers();
 
 app.Run();
